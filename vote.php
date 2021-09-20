4fe2bd2eb5be7efb88f441f724fcfc1259a0ec55
github-haruki <64785351+github-haruki@users.noreply.github.com>
2021-09-20 20:17:36 +0900

    4

--- vote.php
+++ vote.php
@@ -16,10 +16,14 @@ if ($file && $count) {
 	  <head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p&display=swap" rel="stylesheet">
 		<title>送信完了</title>
 		<style type="text/css">
 			body{
 				text-align: center;
+				font-family: 'M PLUS 1p', sans-serif;
 			}
 			h1{
 				text-align: center;
